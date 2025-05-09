c0b8c0d49d2a013db5c512708d26add42c7a5c51
LucasAlfeu <lucas.a1feu19@gmail.com>
2025-05-09 10:41:22 -0300

    Lógica para cadastrar uma nova notícia

--- src/app/components/cadastrar-noticia/cadastrar-noticia.component.ts
+++ src/app/components/cadastrar-noticia/cadastrar-noticia.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { TituloComponent } from '../titulo/titulo.component';
+import { FormsModule } from '@angular/forms';
+import { ApiNoticiasService } from '../../services/api-noticias.service';
 
 @Component({
   selector: 'app-cadastrar-noticia',
-  imports: [TituloComponent],
+  imports: [TituloComponent, FormsModule],
   templateUrl: './cadastrar-noticia.component.html',
   styleUrl: './cadastrar-noticia.component.css'
 })
 export class CadastrarNoticiaComponent {
+  novaNoticia: any = {  };
+  mensagemError: string = '';
+  mensagemSucesso: string = '';
+  estaCarrregando: boolean = false; 
 
+  constructor(private api: ApiNoticiasService) { }
+
+  criaNoticia(): void {
+    // Validação simples
+    if (!this.novaNoticia.title || !this.novaNoticia.body || !this.novaNoticia.image) {
+      this.mensagemError = 'Campos vazio, revise';
+      return;
+    }
+
+    // Inicia o estado de carregamento
+    this.estaCarrregando = true;
+    this.mensagemError = '';
+    this.mensagemSucesso = '';
+
+    // Chama o serviço para criar o item
+    this.api.createItem(this.novaNoticia).subscribe({
+      next: (createdItem) => {
+        // Sucesso na criação
+        this.mensagemSucesso = 'Notícia criada com sucesso!';
+        this.novaNoticia = {}; // Limpa o formulário
+        this.estaCarrregando = false;
+        
+        // Remove a mensagem de sucesso após 3 segundos
+        setTimeout(() => this.mensagemSucesso = '', 3000);
+      },
+      error: (err) => {
+        // Tratamento de erro
+        this.mensagemError = 'Erro ao criar item. Por favor, tente novamente.';
+        this.estaCarrregando = false;
+        console.error('Erro detalhado:', err); // Log para desenvolvimento
+      }
+    });
+  }
 }
--- src/app/services/api-noticias.service.ts
+++ src/app/services/api-noticias.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -16,5 +16,10 @@ export class ApiNoticiasService {
 
   getItemById(id: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  createItem(item: any): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(this.apiUrl, item, { headers });
   }
 }
